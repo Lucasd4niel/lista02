b45cbdc2f5ea27f4a6c8fec942b9bb7a95f8079c
Lucas Fernandes <lucasdanieldmx@gmail.com>
2025-05-07 22:21:45 -0300

    atualizado

--- ex13.c
+++ ex13.c
@@ -3,22 +3,61 @@
 int main(void){
     int x, y, z, maior, inter, men;
     scanf("%d %d %d", &x, &y, &z);
-    if (x > y && x > z){
+    
+    if (x >= y && y >= z)
+    {
         maior = x;
-    }else if(x > y && x < z){
-        inter = x;
-    }else if(x < y && x < z){
-        men = x;
-    }else if (x == y || x == z){
-        if (x > z){
-            men = z;
-            inter = y;
+        inter = y;
+        men = z;
+    }else
+    {
+        if (x >= z  && z >= y)
+        {
             maior = x;
-        }else{
-            men = x;
-            inter = y;
-            maior = z;
+            inter = z;
+            men = y;
+        }
+        else
+        {
+            if(y >= x && x >= z)
+            {
+                maior = y;
+                inter = x;
+                men = z;
+            }
+            else
+            {
+                if(y >= z && z >= x)
+                {
+                    maior = y;
+                    inter = z;
+                    men = x;
+                }
+                else
+                {
+                    if (z >= x && x >= y)
+                    {
+                        maior = z;
+                        inter = x;
+                        men = y;
+                    }
+                    else
+                    {
+                        if (z >= y && y >= x){
+                            maior = z;
+                            inter = y;
+                            men = x;
+                        }
+                    }
+                }      
+            }
         }
     }
+
+    x = men;
+    y = inter;
+    z = maior;
+
+    printf("x = %d, y = %d, z = %d\n", x, y, z);
     return 0;
 }
